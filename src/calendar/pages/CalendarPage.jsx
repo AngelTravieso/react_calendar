4f3cff56a0953b323d14c34a2cae68c69517d6d0
AngelTravieso <angeltraviesoc@gmail.com>
2022-11-19 12:43:56 -0400

    escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -4,6 +4,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
 import { Navbar, CalendarEvent } from "../"
 import { localizer, getMessagesEs } from '../../helpers';
+import { useState } from 'react';
 
 
 const events = [{
@@ -21,6 +22,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = ( evt, start, end, isSelected ) => {
     // console.log({evt, start, end, isSelected});
 
@@ -36,6 +39,19 @@ export const CalendarPage = () => {
     }
   }
 
+
+  const onDoubleClick = ( evt ) => {
+    console.log({doubleClic: evt})
+  }
+
+  const onSelect = ( evt ) => {
+    console.log({click: evt})
+  }
+
+  const onViewChange = ( evt ) => {
+    localStorage.setItem('lastView', evt);
+  }
+
   return (
     <>
       <Navbar />
@@ -44,6 +60,8 @@ export const CalendarPage = () => {
         culture='es'
         localizer={ localizer }
         events={ events }
+        // vista que se carga por default
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px)' }}
@@ -52,6 +70,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChange }
       />
       
     </>
